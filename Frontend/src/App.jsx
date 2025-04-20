da2714e21136c053238c6f1d72bc59ce0df26a11
CYIZERE Happy <happycyizere69@gmail.com>
2025-04-20 21:58:19 +0200

    Update Frontend/src/App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import Navbar from './components/Navbar/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Cart from './pages/Cart/Cart';
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
+import Footer from './components/footer/Footer';
+import LoginPopup from './components/LoginPopup/LoginPopup';
+import NotFound from './pages/404Code/NotFound';
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import TourGuide from './components/webTourGuide/TourGuide';
+import PrivacyPolicy from './pages/Privacy-Policy/privacyPolicy';
+import Verify from './pages/Verify/Verify';
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0); 
+  }, [pathname]);
+
+  return null;
+};
+
+const App = () => {
+  const [showLogin, setShowLogin] = useState(false);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <TourGuide />
+      <ToastContainer />
+      {showLogin && <LoginPopup setShowLogin={setShowLogin} />}
+      <Navbar setShowLogin={setShowLogin} />
+      <div className='app'>
+        <ScrollToTop />
+        <Routes>
+          <Route path='/' element={<Home setShowLogin={setShowLogin} />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/order' element={<PlaceOrder />} />
+          <Route path='*' element={<NotFound />} />
+          <Route path='/privacy-policy' element={<PrivacyPolicy />}></Route>
+          <Route path='/verify' element={<Verify />}></Route>
+        </Routes> 
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
